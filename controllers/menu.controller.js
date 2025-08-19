fe69428d3ef22706ea47b8e881372df2258b93ec
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:10:21 +0300

    menu Show

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -18,3 +18,19 @@ export async function menuIndex(req, res) {
     res.status(500).json({ error: err.message })
   }
 }
+
+export async function menuShow(req, res) {
+  try {
+    const id = req.params.id
+    const item = await Menu.findById(id)
+
+    if (!item) {
+      return res.status(404).json({ error: 'Not found' })
+    }
+
+    res.status(200).json(item)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
