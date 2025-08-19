6aab1891a7251dd9067b645e9c60ec4a5d2170fc
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:03:52 +0300

    Menu Model

---
+++ models/menu.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const MenuSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  slug: { type: String, unique: true },
+  description: String,
+  imageUrl: String,
+  category: { type: String, required: true },
+  price: { type: Number, required: true }
+}, { timestamps: true });
+
+export default mongoose.model('Menu', MenuSchema);
