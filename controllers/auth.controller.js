b70fd837dad6c749d61e272e8e847f3a82dab95f
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:40:17 +0300

    adjustments

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,7 +18,7 @@ export async function register(req, res) {
     const user = await User.create({ username, passwordHash })
 
     res.status(201).json({ id: user._id, username: user.username })
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({ message: 'Server error' })
   }
 }
@@ -37,7 +37,7 @@ export async function login(req, res) {
     const payload = { id: user._id, username: user.username }
     const token = jwt.sign(payload, SECRET, { expiresIn: EXPIRES_IN })
     res.status(200).json({ token })
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({ message: 'Server error' })
   }
 }
