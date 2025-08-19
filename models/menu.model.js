361ddff3e6a7ba9013c445f1a6039ac37608143e
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:06:26 +0300

    Menu Model

--- models/menu.model.js
+++ models/menu.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const MenuSchema = new mongoose.Schema({
   name: { type: String, required: true },
-//   slug: { type: String, unique: true },add
+  slug: { type: String, unique: true },
   description: String,
   imageUrl: String,
   category: { type: String, required: true },
