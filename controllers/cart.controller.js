afc3fbc9a9f5fcc9240acee6e2d012361437f265
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:23:33 +0300

    cart Get

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,2 +1,11 @@
 import Cart from '../models/cart.model.js'
 
+export async function cartGet(req, res) {
+  try {
+    const userId = req.params.userId
+    const found = await Cart.findOne({ user: userId })
+    const cart = found || await Cart.create({ user: userId, items: [] })
+    res.json(cart)
+  } catch (e) { res.status(500).json({ error: e.message }) }
+}
+
