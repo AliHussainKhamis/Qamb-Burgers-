ba7d1709a48b34eb067e61857bbb8ef2b2f6d75c
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:09:07 +0300

    menu Index

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -1,2 +1,10 @@
 import Menu from '../models/menu.model.js'
 
+export async function menuIndex(req, res) {
+  try {
+    const items = await Menu.find(filter)
+    res.status(200).json(items)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
