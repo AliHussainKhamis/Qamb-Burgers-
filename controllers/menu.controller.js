5732602c4397a18ef1bdb75925b81bdf7673ece0
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:09:45 +0300

    Filtering on Index

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -2,6 +2,16 @@ import Menu from '../models/menu.model.js'
 
 export async function menuIndex(req, res) {
   try {
+    const filter = {}
+
+    if (req.query && req.query.category) {
+      filter.category = req.query.category
+    }
+
+    if (req.query && req.query.q) {
+      filter.name = { $regex: req.query.q, $options: 'i' }
+    }
+
     const items = await Menu.find(filter)
     res.status(200).json(items)
   } catch (err) {
