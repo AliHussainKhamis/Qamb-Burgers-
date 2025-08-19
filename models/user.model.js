e402b87374d6ccf3ba46983b9f2806e033f67726
AliHussainKhamis <alihussainkh3@gmail.com>
2025-08-19 11:18:27 +0300

    user Model

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    passwordHash: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+UserSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.passwordHash)
+}
+
+export default mongoose.model('User', UserSchema)
